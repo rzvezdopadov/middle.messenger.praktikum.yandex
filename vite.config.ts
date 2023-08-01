e47c0a7b968d8171b6efd762b3aa8ff93c0ccb93
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-08-01 18:51:19 +0300

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
-import handlebars from "./vite-plugin-handlebars-precompile";
+// import { defineConfig } from "vite";
+// import handlebars from "./vite-plugin-handlebars-precompile";
 
-export default defineConfig({
-	plugins: [handlebars()],
-});
+// export default defineConfig({
+// 	plugins: [handlebars()],
+// });
+
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
+export default {
+	plugins: [
+		handlebars({
+			partialDirectory: resolve(__dirname, "src"),
+		}),
+	],
+};
