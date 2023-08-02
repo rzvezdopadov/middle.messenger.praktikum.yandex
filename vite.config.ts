d466bdb2b3ba1c8cd842fecd98f95b44f3a703ac
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-08-02 17:45:50 +0300

    change files

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,24 @@ import handlebars from "vite-plugin-handlebars";
 export default {
 	plugins: [
 		handlebars({
-			partialDirectory: resolve(__dirname, "src"),
+			partialDirectory: resolve(__dirname, "./src/components/basic"),
 		}),
 	],
+	build: {
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, "index.html"),
+				404: resolve(__dirname, "./src/components/pages/404/404.html"),
+				500: resolve(__dirname, "./src/components/pages/500/500.html"),
+				auth: resolve(__dirname, "./src/components/pages/auth/auth.html"),
+				chats: resolve(__dirname, "./src/components/pages/chats/chats.html"),
+				profile: resolve(__dirname, "./src/components/pages/profile/profile.html"),
+				profile_edit_pwd: resolve(
+					__dirname,
+					"./src/components/pages/profile_edit_pwd/profile_edit_pwd.html"
+				),
+				signin: resolve(__dirname, "./src/components/pages/signin/signin.html"),
+			},
+		},
+	},
 };
