3a2c9c1f785adff4c4b7e4ab1005583be9409a8a
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-08-03 14:38:57 +0300

    change main on the helper

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,25 @@ export default {
 	plugins: [
 		handlebars({
 			partialDirectory: resolve(__dirname, "./src/components/basic"),
+			helpers: {
+				main({ fn }) {
+					return `<!DOCTYPE html>
+					<html lang="ru">
+						<head>
+							<meta charset="UTF-8" />
+							<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+							<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+							<title>Чат</title>
+							<link rel="stylesheet" href="/src/styles/styles.scss" />
+							<link rel="icon" type="image/svg+xml" href="/vite.svg" />
+							
+						</head>
+						<body>
+							<div id="root">${fn(this)}</div>
+						</body>
+					</html>`;
+				},
+			},
 		}),
 	],
 	build: {
